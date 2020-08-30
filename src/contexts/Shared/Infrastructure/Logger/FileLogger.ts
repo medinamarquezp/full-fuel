f7e578b2b0fb0debf1e1be626043301e419caf72
Pedro Medina <medinamarquezp@gmail.com>
2020-08-30 13:26:26 +0200

    refactor: decoupled Winston dependency on FileLogger class

--- src/contexts/Shared/Infrastructure/Logger/FileLogger.ts
+++ src/contexts/Shared/Infrastructure/Logger/FileLogger.ts
@@ -1,40 +1,30 @@
 /* eslint-disable prefer-const */
 import { Logger } from "@/sharedDomain/Logger";
-import { createLogger, format, transports } from "winston";
-
-const logsFolderPath = `${process.cwd()}/logs`;
-const { combine, timestamp, simple, printf } = format;
+import { WinstonLogger } from "./WinstonLogger";
 
 export let FileLogger: Logger;
 
 FileLogger = class
 {
-  private static logger = createLogger({
-    exitOnError: false,
-    format: combine(
-      simple(),
-      timestamp(),
-      printf(info => `[${info.timestamp}] (${__filename.split(/[\\/]/).pop()}) - ${info.level.toUpperCase()} - ${info.message}`)
-    ),
-    transports: [
-      new transports.File({ filename: `${logsFolderPath}/info.log`, level: "info" }),
-      new transports.File({ filename: `${logsFolderPath}/warn.log`, level: "warn" }),
-      new transports.File({ filename: `${logsFolderPath}/error.log`, level: "error" })
-    ]
-  })
+  private static logger()
+  {
+    const logsFolderPath = `${process.cwd()}/logs`;
+    const loggerInstance = new WinstonLogger(logsFolderPath);
+    return loggerInstance.winstonLogger();
+  }
 
   static info(message: string): void
   {
-    this.logger.info(message);
+    this.logger().info(message);
   }
 
   static warn(message: string): void
   {
-    this.logger.warn(message);
+    this.logger().warn(message);
   }
 
   static error(message: string): void
   {
-    this.logger.error(message);
+    this.logger().error(message);
   }
 };
---
+++ src/contexts/Shared/Infrastructure/Logger/WinstonLogger.ts
@@ -0,0 +1,49 @@
+import { TransformableInfo } from "logform";
+import { createLogger, format, transports, Logger } from "winston";
+const { combine, timestamp, simple, printf } = format;
+
+export class WinstonLogger
+{
+  private logsFolderPath: string;
+
+  constructor(logsFolderPath: string)
+  {
+    this.logsFolderPath = logsFolderPath;
+  }
+
+  public winstonLogger(): Logger
+  {
+    return createLogger({
+      exitOnError: false,
+      format: combine(
+        simple(),
+        timestamp(),
+        printf(info => this.logContent(info))
+      ),
+      transports: this.createTransportList(["info", "warn", "error"])
+    });
+  }
+
+  private createTransportFile(level: string): transports.FileTransportInstance
+  {
+    return new transports.File({ filename: `${this.logsFolderPath}/${level}.log`, level: `${level}` });
+  }
+
+  private createTransportList(levels: string[]): transports.FileTransportInstance[]
+  {
+    const transports: transports.FileTransportInstance[] = [];
+
+    for (const level of levels)
+    {
+      const transportFile = this.createTransportFile(level);
+      transports.push(transportFile);
+    }
+    return transports;
+  }
+
+  private logContent(info: TransformableInfo): string
+  {
+    return `[${info.timestamp}] (${__filename.split(/[\\/]/).pop()}) - ${info.level.toUpperCase()} - ${info.message}`;
+  }
+
+}
