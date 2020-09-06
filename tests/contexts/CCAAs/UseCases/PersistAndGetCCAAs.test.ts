b19f62ed857e6bafb294179811748bd26c9b4822
Pedro Medina <medinamarquezp@gmail.com>
2020-09-06 19:48:23 +0200

    test: tested get and persist CCAAs use cases using in memory repo

---
+++ tests/contexts/CCAAs/UseCases/PersistAndGetCCAAs.test.ts
@@ -0,0 +1,25 @@
+import { CCAAListMock } from "../Domain/CCAAList.mock"
+import { PersistCCAAs } from "@/contexts/CCAAs/UseCases/PersistCCAAs";
+import { GetAllCCAAs } from "@/contexts/CCAAs/UseCases/GetAllCCAAs";
+import { InMemoryCCAARepo } from "@/contexts/CCAAs/Infrastructure/Persistence/InMemoryCCAARepo";
+
+describe('Persist and get all CCAA use case', () =>
+{
+  let inMemory: InMemoryCCAARepo, persistCCAAs: PersistCCAAs, CCAAs: GetAllCCAAs;
+  beforeEach(() =>
+  {
+    inMemory = new InMemoryCCAARepo();
+    persistCCAAs = new PersistCCAAs(inMemory);
+    CCAAs = new GetAllCCAAs(inMemory);
+  })
+  test('Persist and get CCAAs', async () =>
+  {
+    const totalCCAAs = 19;
+    await persistCCAAs.persist(CCAAListMock);
+    const persistedData = await CCAAs.get();
+    const firstCCAA = persistedData[0];
+    expect(persistedData.length).toBe(totalCCAAs);
+    expect(firstCCAA.getExternalID()).toBe("01");
+    expect(firstCCAA.getName()).toBe("Andalucia");
+  })
+})
