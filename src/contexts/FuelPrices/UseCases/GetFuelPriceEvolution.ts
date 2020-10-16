c282d750936f83142b4497a47665f524a1a31bba
Pedro Medina <medinamarquezp@gmail.com>
2020-10-16 23:57:44 +0200

    style: rename upper camel case method

--- src/contexts/FuelPrices/UseCases/GetFuelPriceEvolution.ts
+++ src/contexts/FuelPrices/UseCases/GetFuelPriceEvolution.ts
@@ -3,7 +3,7 @@ import { FuelPriceRepo } from "@/contexts/FuelPrices/Domain/FuelPriceRepo";
 import { FuelTypes } from "@/contexts/FuelPrices/Domain/FuelTypes";
 import { FuelPriceEvolution } from "@/contexts/FuelPrices/Domain/FuelPriceEvolution";
 
-export class getFuelPriceEvolution extends BaseUseCase{
+export class GetFuelPriceEvolution extends BaseUseCase{
   constructor(private repository: FuelPriceRepo) { super(); }
 
   async getEvolution(fuelstationID: number, fuelType: FuelTypes, price: number): Promise<FuelPriceEvolution> {
