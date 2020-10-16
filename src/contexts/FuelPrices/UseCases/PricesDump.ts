255bf5456aab3f11d23ea94bc7fed672f6f25212
Pedro Medina <medinamarquezp@gmail.com>
2020-10-17 00:23:53 +0200

    feat: prices dump use case created

---
+++ src/contexts/FuelPrices/UseCases/PricesDump.ts
@@ -0,0 +1,17 @@
+import { BaseUseCase } from "@/sharedUseCases/BaseUseCase";
+import { FuelPriceRepo } from "@/contexts/FuelPrices/Domain/FuelPriceRepo";
+import { FuelTypes } from "@/contexts/FuelPrices/Domain/FuelTypes";
+import { FuelPriceStatisticsType } from "@/contexts/FuelPrices/Domain/FuelPriceStatistics";
+
+export class PricesDump extends BaseUseCase{
+  constructor(private repository: FuelPriceRepo) { super(); }
+
+  async dump(fuelstationID: number, fuelType: FuelTypes, priceStatistics: FuelPriceStatisticsType): Promise<void> {
+    try {
+      await this.repository.pricesDump(fuelstationID, fuelType, priceStatistics);
+      this.logger.info(`Price for ${FuelTypes} of fuelstation ${fuelstationID} has been dumping correctly`);
+    } catch (error) {
+      this.logger.error(`Error when dumping ${FuelTypes} prices of fuel station ${fuelstationID}`);
+    }
+  }
+}
