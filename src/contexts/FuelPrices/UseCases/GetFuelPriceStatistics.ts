6c59113abdfb9532aab4711f40bdf3ab3d9987f7
Pedro Medina <medinamarquezp@gmail.com>
2020-10-17 00:23:30 +0200

    feat: get fuel price statistics use case created

---
+++ src/contexts/FuelPrices/UseCases/GetFuelPriceStatistics.ts
@@ -0,0 +1,19 @@
+import { BaseUseCase } from "@/sharedUseCases/BaseUseCase";
+import { FuelPriceRepo } from "@/contexts/FuelPrices/Domain/FuelPriceRepo";
+import { FuelTypes } from "@/contexts/FuelPrices/Domain/FuelTypes";
+import { FuelPriceStatisticsType } from "@/contexts/FuelPrices/Domain/FuelPriceStatistics";
+
+export class GetFuelPriceStatistics extends BaseUseCase{
+  constructor(private repository: FuelPriceRepo) { super(); }
+
+  async getStatistics(fuelstationID: number, fuelType: FuelTypes): Promise<FuelPriceStatisticsType> {
+    let priceStatistics = { min: 0, max: 0, avg: 0 };
+
+    try {
+      priceStatistics = await this.repository.getPriceStatistics(fuelstationID, fuelType);
+    } catch (error) {
+      this.logger.error(`Error on retrieving price statistics for ${fuelType} on ${fuelstationID}`);
+    }
+    return priceStatistics;
+  }
+}
