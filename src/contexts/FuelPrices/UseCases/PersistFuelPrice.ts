8ac1f89723dc94875957b3bec1f8f798cb0caa9d
Pedro Medina <medinamarquezp@gmail.com>
2020-10-16 23:47:10 +0200

    feat: persist fuel price use case created

---
+++ src/contexts/FuelPrices/UseCases/PersistFuelPrice.ts
@@ -0,0 +1,16 @@
+import { BaseUseCase } from "@/sharedUseCases/BaseUseCase";
+import { FuelPriceRepo } from "@/contexts/FuelPrices/Domain/FuelPriceRepo";
+import { FuelPrice } from "@/contexts/FuelPrices/Domain/FuelPrice";
+
+export class PersistFuelPrice extends BaseUseCase{
+  constructor(private repository: FuelPriceRepo) { super(); }
+
+  async persist(fuelPrice: FuelPrice): Promise<void> {
+    try {
+      await this.repository.save(fuelPrice);
+      this.logger.info(`Fuel price for ${fuelPrice.fuelType} on ${fuelPrice.fuelstationID} has been persisted correctly`);
+    } catch (error) {
+      this.logger.error(`Error on persisting a fuel price. ${error}`);
+    }
+  }
+}
