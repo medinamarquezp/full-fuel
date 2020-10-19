bc57b5ad479b9a58bceb22ead3ea59618964adfd
Pedro Medina <medinamarquezp@gmail.com>
2020-10-20 01:55:18 +0200

    test: tests of subscriptions use cases

---
+++ tests/contexts/Subscriptions/AddSubscriptions.test.ts
@@ -0,0 +1,25 @@
+import { FuelTypes } from "@/sharedDomain/FuelTypes";
+import { AddSubscriptions } from "@/contexts/Subscriptions/UseCases/AddSubscriptions";
+import { InMemorySubscriptions } from "@/contexts/Subscriptions/Infrastructure/Persistence/InMemorySubscriptions";
+
+describe('Add subscriptions use case test', () => {
+  let addSubscriptions: AddSubscriptions, repo: InMemorySubscriptions;
+  beforeAll(() => {
+    repo = new InMemorySubscriptions();
+    addSubscriptions = new AddSubscriptions(repo);
+  })
+  test('it should add a new subscription to repository', async() => {
+    addSubscriptions.add(1234, FuelTypes.G95);
+    const sut = await repo.getSubscriptions();
+    expect(sut.length).toBe(1);
+    expect(sut[0].numSubscriptions).toBe(1);
+  })
+  test('it should increment subscriptions number to an existing fuel station', async() => {
+    addSubscriptions.add(4567, FuelTypes.G95);
+    addSubscriptions.add(4567, FuelTypes.G95);
+    addSubscriptions.add(4567, FuelTypes.G95);
+    const sut = await repo.getSubscriptions();
+    expect(sut.length).toBe(2);
+    expect(sut[1].numSubscriptions).toBe(3);
+  })
+})
