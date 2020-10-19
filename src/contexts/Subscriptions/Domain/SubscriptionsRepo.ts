776f916ad7456d4ad8c1469a7ff14c964e396794
Pedro Medina <medinamarquezp@gmail.com>
2020-10-20 00:59:26 +0200

    fix: added parameters to methods

--- src/contexts/Subscriptions/Domain/SubscriptionsRepo.ts
+++ src/contexts/Subscriptions/Domain/SubscriptionsRepo.ts
@@ -1,5 +1,6 @@
+import { FuelTypes } from "@/sharedDomain/FuelTypes";
 export interface SubscriptionsRepo {
-  addSubscribers(): void,
-  removeSubscribers(): void,
-  getSubscribers(): number
+  addSubscriptions(fuelstationID: number, fuelType: FuelTypes): Promise<void>,
+  removeSubscriptions(fuelstationID: number, fuelType: FuelTypes): Promise<void>,
+  getSubscriptions(fuelstationID: number, fuelType: FuelTypes): Promise<number>
 }
