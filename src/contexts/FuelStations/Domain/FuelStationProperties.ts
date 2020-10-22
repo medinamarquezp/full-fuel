4210439a6e07d37a5dc222178b3d79e122352b09
Pedro Medina <medinamarquezp@gmail.com>
2020-10-22 23:35:11 +0200

    feat: fuelstation properties

---
+++ src/contexts/FuelStations/Domain/FuelStationProperties.ts
@@ -0,0 +1,22 @@
+import { CCAA } from "@/contexts/CCAAs/Domain/CCAA";
+import { FuelPrice } from "@/contexts/FuelPrices/Domain/FuelPrice";
+import { Timetables } from "@/contexts/Timetables/Domain/Timetables";
+
+export interface FuelStationsProperties {
+  fuelstationID: number,
+  ccaa: CCAA,
+  name: string,
+  address: string,
+  postalCode: string,
+  province: string,
+  city: string,
+  town: string,
+  latitude: number,
+  longitude: number,
+  timetable: string,
+  timetables: Timetables[],
+  prices: FuelPrice[],
+  bestDay: number,
+  bestMoment: string,
+  brandImage: string
+}
