5ee434489c2a47f0b7aeae76c52f7478d258a956
Pedro Medina <medinamarquezp@gmail.com>
2020-10-24 18:23:55 +0200

    feat: added relationship

--- src/contexts/CCAAs/Infrastructure/Persistence/CCAAOrmEntity.ts
+++ src/contexts/CCAAs/Infrastructure/Persistence/CCAAOrmEntity.ts
@@ -1,7 +1,8 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Table, Column, CreatedAt, UpdatedAt, Model, Unique, PrimaryKey } from "sequelize-typescript";
+import { Table, Column, CreatedAt, UpdatedAt, Model, Unique, PrimaryKey, HasMany } from "sequelize-typescript";
 import { CCAAProperties } from "@/contexts/CCAAs/Domain/CCAAProperties";
+import { FuelStationOrmEntity } from "@/contexts/FuelStations/Infrastructure/Persistence/FuelStationOrmEntity";
 
 @Table({ timestamps: true, tableName: "ccaas" })
 export class CCAAOrmEntity extends Model<CCAAOrmEntity> implements CCAAProperties
@@ -13,6 +14,9 @@ export class CCAAOrmEntity extends Model<CCAAOrmEntity> implements CCAAPropertie
 
   @Column
   name: string;
+
+  @HasMany(() => FuelStationOrmEntity, "fuelstationID")
+  fuelstations: FuelStationOrmEntity[];
 
   @CreatedAt
   createdAt: Date;
