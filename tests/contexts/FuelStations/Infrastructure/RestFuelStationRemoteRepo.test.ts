9783a26c9a95c7e43a88d60396efe404b718a142
Pedro Medina <medinamarquezp@gmail.com>
2020-10-24 03:12:19 +0200

    test: fetch fuel stations from Minetur test

---
+++ tests/contexts/FuelStations/Infrastructure/RestFuelStationRemoteRepo.test.ts
@@ -0,0 +1,20 @@
+import { MineturEndpoints } from "@/config/MineturEndpoints";
+import { FecthRestClient } from "@/sharedInfrastructure/FetchRestClient";
+import { FuelStation } from "@/contexts/FuelStations/Domain/FuelStation";
+import { RestFuelStationRemoteRepo } from "@/contexts/FuelStations/Infrastructure/Remote/RestFuelStationRemoteRepo";
+
+describe('Fetch fuel stations from Minetur test', () =>
+{
+  let restFromRemote: RestFuelStationRemoteRepo, sut: FuelStation[];
+  beforeAll(async () =>
+  {
+    restFromRemote = new RestFuelStationRemoteRepo(MineturEndpoints.FuelStatinsByIdCCAA, FecthRestClient);
+    sut = await restFromRemote.getFuelStationsByCCAA("04");
+  })
+  test('it should return fuel stations from baleares', async () =>
+  {
+    const firstFuelStation = sut[0];
+    expect(firstFuelStation.ccaa).toBe("04");
+    expect(firstFuelStation.province).toBe("BALEARS (ILLES)");
+  })
+})
