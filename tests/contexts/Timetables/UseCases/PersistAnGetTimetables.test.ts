cfcfd135bd8b4f6bdfdacad9726ddbb5e62faf23
Pedro Medina <medinamarquezp@gmail.com>
2020-10-25 12:18:29 +0100

    fix: updated fuel station id

--- tests/contexts/Timetables/UseCases/PersistAnGetTimetables.test.ts
+++ tests/contexts/Timetables/UseCases/PersistAnGetTimetables.test.ts
@@ -16,7 +16,7 @@ describe('Persist and get timetables use case', () =>
     persistTimetables = new PersistTimetables(inMemory);
     getTimetables = new GetTimetables(inMemory);
     cepsaAlaior = Timetables.timetablesInstancesFromString("L: 07:00-21:00", 2590);
-    repsolAlcudia = Timetables.timetablesInstancesFromString("L-D: 07:00-19:00", 2733);
+    repsolAlcudia = Timetables.timetablesInstancesFromString("L-D: 07:00-19:00", 13088);
   })
   test('Persist and get all timetables', async () =>
   {
@@ -24,13 +24,13 @@ describe('Persist and get timetables use case', () =>
     const allTimetables = await inMemory.getAll();
     expect(allTimetables.length).toBe(8);
     expect(allTimetables[0].fuelstationID).toBe(2590);
-    expect(allTimetables[1].fuelstationID).toBe(2733);
+    expect(allTimetables[1].fuelstationID).toBe(13088);
   })
   test('Persist and get timetables by fuel station ID', async () => {
     await inMemory.save([...cepsaAlaior, ...repsolAlcudia]);
-    const repsolAlcudiatimetables = await inMemory.getByFuelstationID(2733);
+    const repsolAlcudiatimetables = await inMemory.getByFuelstationID(13088);
     expect(repsolAlcudiatimetables.length).toBe(7);
-    expect(repsolAlcudiatimetables[0].fuelstationID).toBe(2733);
+    expect(repsolAlcudiatimetables[0].fuelstationID).toBe(13088);
     expect(repsolAlcudiatimetables[0].weekDay).toBe(0);
     expect(repsolAlcudiatimetables[0].alwaysOpen).toBeFalsy();
     expect(repsolAlcudiatimetables[0].opening).toBe("07:00");
