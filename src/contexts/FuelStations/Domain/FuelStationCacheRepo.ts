d8fc85ba4b0c25e8aa742c8d3d3c5f573fdfeaa8
Pedro Medina <medinamarquezp@gmail.com>
2020-10-30 00:02:55 +0100

    feat: fuel station cache repo

---
+++ src/contexts/FuelStations/Domain/FuelStationCacheRepo.ts
@@ -0,0 +1,7 @@
+import { FuelStation } from "./FuelStation";
+export interface FuelStationCacheRepo {
+  setGeoPoint(longitude: number, latitude: number, fuelstationID: number): Promise<void>;
+  getGeoPoints(longitude: number, latitude: number, radius: number): Promise<(string | [string, string | [string, string]])[]>;
+  addFuelStationToCache(fuelstationID: number, fuelstation: FuelStation): Promise<void>;
+  getFuelStationsFromCache(fuelstationIDs: number[]): Promise<FuelStation[]>;
+}
