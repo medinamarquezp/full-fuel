27ef37fdeabfaf1dc553e177437a3ab603987dae
Pedro Medina <medinamarquezp@gmail.com>
2020-10-30 01:01:35 +0100

    refactor: yagni getOne

--- src/contexts/FuelStations/UseCases/GetFuelStationsFromCache.ts
+++ src/contexts/FuelStations/UseCases/GetFuelStationsFromCache.ts
@@ -5,20 +5,6 @@ export class GetFuelStationsFromCache extends BaseUseCase
 {
   constructor(private repository: FuelStationCacheRepo) { super(); }
 
-  async getOne(fuelstationID: number): Promise<FuelStation>
-  {
-    let fuelStation: FuelStation | undefined;
-
-    try
-    {
-      fuelStation = await this.repository.getFuelStationFromCache(fuelstationID);
-    } catch (err)
-    {
-      this.handleError(`Error on getting fuel station ${fuelstationID} from cache. ${err}`);
-    }
-    return fuelStation as FuelStation;
-  }
-
   async getMany(fuelstationIDs: number[]): Promise<FuelStation[]>
   {
     let fuelStations: FuelStation [] = [];
