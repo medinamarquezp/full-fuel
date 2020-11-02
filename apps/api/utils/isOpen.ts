bcc2009258bb5c85349893f9deb5cd7eeacf0103
Pedro Medina <medinamarquezp@gmail.com>
2020-11-02 02:29:27 +0100

    feat: check if fuelstation is open

---
+++ apps/api/utils/isOpen.ts
@@ -0,0 +1,10 @@
+import { Today } from "@/sharedDomain/Today";
+import { Timetables } from "@/contexts/Timetables/Domain/Timetables";
+
+export const isOpen = (isAlwaysOpen: boolean, timetables: Timetables[]): boolean => {
+  if (isAlwaysOpen) return true;
+  const weekDay = Today.weekDay();
+  const todayTimetable = timetables.find(timetable => timetable.weekDay === weekDay);
+  const { opening, closing } = todayTimetable as Timetables;
+  return Today.isBetween(opening, closing);
+};
