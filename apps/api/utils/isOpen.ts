beace3f83387ddf493de465c5f7a33fe63a10d0f
Pedro Medina <medinamarquezp@gmail.com>
2020-11-04 18:39:48 +0100

    fix: error 500 when fuel station opens a day that is not today

--- apps/api/utils/isOpen.ts
+++ apps/api/utils/isOpen.ts
@@ -5,6 +5,7 @@ export const isOpen = (isAlwaysOpen: boolean, timetables: Timetables[]): boolean
   if (isAlwaysOpen) return true;
   const weekDay = Today.weekDay();
   const todayTimetable = timetables.find(timetable => timetable.weekDay === weekDay);
+  if(!todayTimetable) return false;
   const { opening, closing } = todayTimetable as Timetables;
   return Today.isBetween(opening, closing);
 };
