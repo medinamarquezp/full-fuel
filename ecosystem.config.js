e868cb1e780e873377eb96ea4e73d4c13d9227e5
Pedro Medina Márquez <medinamarquezp@gmail.com>
2020-11-13 09:13:23 +0100

    chore: setted cron jobs on ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,11 +8,28 @@ module.exports = {
     ignore_watch : ["node_modules", "patches", "tests", "logs", "build"],
     autorestart: true
   }, {
-    name: "JOBS",
-    script: "./build/apps/scheduledTasks/scheduledTasks.js",
+    name: "Fuel stations JOB",
+    script: "./build/apps/scheduledTasks/jobs/FuelStationsJob.js",
     exec_mode: "fork",
     watch: true,
     ignore_watch : ["node_modules", "patches", "tests", "logs", "build"],
-    autorestart: true
+    autorestart: false,
+    cron_restart: "0 6,13,19 * * *",
+  }, {
+    name: "Notifications JOB",
+    script: "./build/apps/scheduledTasks/jobs/NotificationsJob.js",
+    exec_mode: "fork",
+    watch: true,
+    ignore_watch : ["node_modules", "patches", "tests", "logs", "build"],
+    autorestart: false,
+    cron_restart: "30 6,13,19 * * *",
+  }, {
+    name: "CCAAs JOB",
+    script: "./build/apps/scheduledTasks/jobs/CCAAJob.js",
+    exec_mode: "fork",
+    watch: true,
+    ignore_watch : ["node_modules", "patches", "tests", "logs", "build"],
+    autorestart: false,
+    cron_restart: "0 0 * * 7",
   }]
 };
