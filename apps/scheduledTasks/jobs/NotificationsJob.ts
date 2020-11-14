b5e9717ff83e9914dc42ab9017da2ff83f6ec994
Pedro Medina Márquez <medinamarquezp@gmail.com>
2020-11-14 11:48:58 +0100

    feat: job execution time validator

--- apps/scheduledTasks/jobs/NotificationsJob.ts
+++ apps/scheduledTasks/jobs/NotificationsJob.ts
@@ -1,6 +1,8 @@
+import { validateJobExecutionTime } from "../utils/validateJobExecutionTime";
 import { NotificationsJobController } from "../controllers/NotificationsJobController";
 
 (async () => {
+  validateJobExecutionTime();
   await NotificationsJobController.run();
   console.log("Notifications process finished correctly");
   process.exit();
