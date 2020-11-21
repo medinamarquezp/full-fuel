07ca70a27e3973342796d2a8b989d90102d91b72
Pedro Medina Márquez <medinamarquezp@gmail.com>
2020-11-21 13:40:23 +0100

    fix: improvements of validations in job execution

--- apps/scheduledTasks/jobs/NotificationsJob.ts
+++ apps/scheduledTasks/jobs/NotificationsJob.ts
@@ -1,8 +1,10 @@
 import { validateJobExecutionTime } from "../utils/validateJobExecutionTime";
 import { NotificationsJobController } from "../controllers/NotificationsJobController";
+import { validateIfJobExecuted } from "../utils/validateIfJobExecuted";
 
 (async () => {
   validateJobExecutionTime();
+  await validateIfJobExecuted();
   await NotificationsJobController.run();
   console.log("Notifications process finished correctly");
   process.exit();
