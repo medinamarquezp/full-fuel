573c6f7c1f25aa5889add2c2fc0282d48fef453d
Pedro Medina Márquez <medinamarquezp@gmail.com>
2020-11-21 12:45:00 +0100

    feat: its execute fuel stations job every 15 minutes to prevent remote errors

--- apps/scheduledTasks/jobs/FuelStationsJob.ts
+++ apps/scheduledTasks/jobs/FuelStationsJob.ts
@@ -1,11 +1,13 @@
 import { resolve, join } from "path";
 import { processByCCAA } from "../utils/processByCCAA";
 import { validateJobExecutionTime } from "../utils/validateJobExecutionTime";
+import { validateIfJobExecuted } from "../utils/validateIfJobExecuted";
 
 const rootDir = resolve(process.cwd());
 const processFile = join(rootDir, "build", "apps", "scheduledTasks", "controllers", "FuelStationJobController.js");
 
 (async () => {
   validateJobExecutionTime();
+  await validateIfJobExecuted();
   await processByCCAA(processFile);
 })();
